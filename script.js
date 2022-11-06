db0a835df72eba8c3aa68b8483490322a7adc03e
M <unmykx@gmail.com>
2022-11-06 07:12:30 -0800

    added func to find nearest element with reduce

--- script.js
+++ script.js
@@ -12,8 +12,25 @@ draggables.forEach(draggable => {
 })
 
 containers.forEach(container => {
-    container.addEventListener('dragover', () => {
-       const draggable = document.querySelector('.dragging')
-       container.appendChild(draggable)
+    container.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(container, e.clientY)
+        console.log(afterElement)
+        const draggable = document.querySelector('.dragging')
+        container.appendChild(draggable)
     })
 })
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = y - box.top - box.height / 2 
+        if(offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
